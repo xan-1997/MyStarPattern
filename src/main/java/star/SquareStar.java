5e8e5398edb8ea5fe2049f987d2a0a895431fe4f
Xanlar7 <xan-1997>
2021-08-28 13:19:43 +0400

    first commit

---
+++ src/main/java/star/SquareStar.java
@@ -0,0 +1,43 @@
+package star;
+
+import java.util.Scanner;
+
+public class SquareStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        getSquareStar(length);
+    }
+
+    //kvadrat ulduzun capa verilmesi
+    static void getSquareStar(int lengthOfStar) {
+        if(lengthOfStar<3){
+            System.out.println("Uzunluq 3 den cox olmalidir");
+            return;
+        }
+
+        if (lengthOfStar%2==0){
+            System.out.println("Uzunluq tek reqem olmalidir");
+            return;
+        }
+
+        String[][] ulduz = new String[lengthOfStar][lengthOfStar];
+        int numberOfSpaces = lengthOfStar / 2;  //probellerin sayi
+
+        for (int x = 0; x < lengthOfStar; x++) {
+            for (int y = 0; y < lengthOfStar; y++) {
+
+                int numberOfStars = lengthOfStar - Math.abs(numberOfSpaces); //ulduzlarin sayi
+                if (y >= Math.abs(numberOfSpaces) && y < numberOfStars)
+                    System.out.print(ulduz[x][y] = " * ");
+                else
+                    System.out.print(ulduz[x][y] = "   ");
+            }
+            numberOfSpaces--;
+            System.out.println();
+        }
+    }
+
+
+}
+
